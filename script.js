70fe24819ae6253fd5c376f07a8ecdc1393a4b5a
Dzung Nguyen <dzung_min@yahoo.com>
2020-06-26 02:32:14 +0700

    Version 2 (not responsive yet)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function playRound(playerSelection, computerSelection) {
   'use strict';
-  if (playerSelection.toLowerCase() === 'rock') {
+  if (playerSelection === 'rock') {
     switch (computerSelection) {
       case 'rock':
         return 'Draw';
@@ -9,7 +9,7 @@ function playRound(playerSelection, computerSelection) {
       case 'scissors':
         return "You Win! Rock beats Scissors";
     }
-  } else if (playerSelection.toLowerCase() === 'paper') {
+  } else if (playerSelection === 'paper') {
     switch (computerSelection) {
       case 'paper':
         return 'Draw';
@@ -18,7 +18,7 @@ function playRound(playerSelection, computerSelection) {
       case 'scissors':
         return "You Lose! Scissors beats Paper";
     }
-  } else if (playerSelection.toLowerCase() === 'scissors') {
+  } else if (playerSelection === 'scissors') {
     switch (computerSelection) {
       case 'scissors':
         return 'Draw';
@@ -43,33 +43,49 @@ function computerPlay() {
   }
 }
 
-function game() {
-  'use strict';
-  let score = 0;
-  let choices = ["rock", "paper", "scissors"];
-  let playerSelection;
-  for (let i = 0; i < 5; i++) {
-    do {
-      playerSelection = prompt("Enter your choice:").toLowerCase();
-    } while (choices.indexOf(playerSelection) === -1);
-    
-    let computerSelection = computerPlay();
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    if (result.indexOf('Win') !== -1) {
-      score++;
-    } else if (result.indexOf('Lose') !== -1) {
-      score--;
+
+const container = document.querySelector('#container');
+const btnList = document.querySelectorAll('.playerChoice');
+const roundResult = document.querySelector('#round-result');
+const score = document.querySelector('#score');
+const finalResult = document.querySelector('#final-result');
+const resetBtn = document.querySelector('#reset-btn');
+
+let playerSelection = 0,
+  playerScore = 0,
+  computerSelection = 0,
+  computerScore = 0,
+  result = 0;
+score.textContent = `${playerScore} - ${computerScore}`;
+roundResult.textContent = 'Make your choice!';
+
+btnList.forEach(btn => {
+  btn.addEventListener('click', () => {
+    playerSelection = btn.value;
+    computerSelection = computerPlay();
+    result = playRound(playerSelection, computerSelection);
+
+    if (result.indexOf('Win') !== -1) playerScore++;
+    else if (result.indexOf('Lose') !== -1) computerScore++;
+
+    score.textContent = `${playerScore} - ${computerScore}`;
+    roundResult.textContent = result;
+
+    if (playerScore === 5 || computerScore === 5) {
+      finalResult.textContent = playerScore === 5 ? 'You win!' : 'You lose!';
+      btnList.forEach(btn => {
+        btn.setAttribute('disabled', 'true');
+      })
     }
-  }
-
-  if (score > 0) {
-    console.log("YOU WIN!");
-    return;
-  } else {
-    console.log("COMPUTER WIN!");
-    return;
-  }
-}
-
-game();
+    resetBtn.addEventListener('click', () => {
+      playerScore = 0;
+      computerScore = 0;
+      btnList.forEach(btn => {
+        btn.removeAttribute('disabled');
+      })
+      score.textContent = `${playerScore} - ${computerScore}`;
+      roundResult.textContent = 'Make your choice!';
+      finalResult.textContent = '';
+    })
+  })
+})
